cb522c5b5ed8707120fe821813021d1feb1c98bd
Felix Vega <felix@polygonag.com>
2024-02-21 16:45:21 -0600

    Add WhatsApp icon to the page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,27 @@ import FeaturedServices from "@/components/Sections/FeaturedServices";
 import HeroSection from "@/components/Sections/HeroSection";
 import RentSection from "@/components/Sections/RentSection";
 
+import WhatsApp from "@/public/images/WhatsApp Icon.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex flex-col gap-20">
+    <main className="relative flex flex-col gap-20">
       <HeroSection />
       <FeaturedClients />
       <FeaturedServices />
       <RentSection />
+      <div className="sticky bottom-0 right-0 w-fit rounded-md bg-[#FC9532] p-4">
+        <div className="relative h-[74px] w-[356px]">
+          <Image
+            className="object-cover object-center"
+            sizes="100vw"
+            alt=""
+            fill
+            src={WhatsApp}
+          />
+        </div>
+      </div>
     </main>
   );
 }
