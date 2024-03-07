efd5505c869377fcbf8d1455317cd796e1e99b1d
Felix Vega <felix@polygonag.com>
2024-03-06 18:28:01 -0600

    Update pricing table styling and display currency in uppercase

--- components/Table/PlaceTablePricing.tsx
+++ components/Table/PlaceTablePricing.tsx
@@ -7,7 +7,7 @@ export default function PlaceTablePricing({
 }) {
   return (
     <div className="bg-gray-100 p-12">
-      <h2 className="mb-4 text-center text-6xl font-bold">Precios</h2>
+      <h2 className="mb-12 text-center text-6xl font-bold">Precios</h2>
       <div className="w-full overflow-x-auto">
         <table className="w-full border-spacing-7 text-3xl">
           <thead>
@@ -15,7 +15,7 @@ export default function PlaceTablePricing({
               <th className="text-center">Día</th>
               {prices[0].type.map((priceType) => (
                 <th key={priceType.type} className="text-center">
-                  {priceType.type}
+                  {priceType.type.toUpperCase()}
                 </th>
               ))}
             </tr>
@@ -25,7 +25,7 @@ export default function PlaceTablePricing({
               <tr key={priceInfo.day} className="border-b">
                 <td className="text-center font-semibold">{priceInfo.day}</td>
                 {priceInfo.type.map((priceType) => (
-                  <td key={priceType.type} className="text-center">
+                  <td key={priceType.type} className="py-4 text-center">
                     {formatCurrency(priceType.price)}
                   </td>
                 ))}
