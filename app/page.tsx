bf2758ef9e727395a9ff748ff74e3175e6da0a40
Felix Vega <felix@polygonag.com>
2024-03-06 18:31:01 -0600

    Refactor iframe element to be responsive

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,15 @@ export default function Home() {
         <RentSection />
         <ContactForm />
       </div>
-      <iframe
-        className="mx-auto"
-        src="https://calendar.google.com/calendar/embed?src=c_b8a50ca9fdb33abe461d74c935c0a544a3ae2c9aff5291b18a14cae55389ee86%40group.calendar.google.com&ctz=America%2FMexico_City"
-        width="800"
-        height="600"
-      ></iframe>
+      <div className="aspect-square w-full overflow-x-scroll">
+        <iframe
+          className="mx-auto h-96 w-11/12 lg:h-96 lg:w-3/4"
+          src="https://calendar.google.com/calendar/embed?src=c_b8a50ca9fdb33abe461d74c935c0a544a3ae2c9aff5291b18a14cae55389ee86%40group.calendar.google.com&ctz=America%2FMexico_City"
+          width="800"
+          height="600"
+        ></iframe>
+      </div>
+
       <Link
         href={
           "https://wa.me/5513842959?text=Hola,%20me%20gustaría%20saber%20más%20sobre%20sus%20servicios"
