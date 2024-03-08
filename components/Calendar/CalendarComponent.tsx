1492de0393fe00a6a4afc8db2a911d490f4977a7
Felix Vega <felix@polygonag.com>
2024-03-08 17:33:13 -0600

    Update dot colors in CalendarComponent

--- components/Calendar/CalendarComponent.tsx
+++ components/Calendar/CalendarComponent.tsx
@@ -9,46 +9,47 @@ export const CalendarComponent = () => {
     {
       name: "La Galería",
       url: "https://calendly.com/amphitryonterrazas/la-galeria?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=000409",
+      dotColor: "#A6E0DA",
     },
     {
       name: "Macarella",
       url: "https://calendly.com/amphitryonterrazas/macarella?hide_event_type_details=1&hide_gdpr_banner=1&text_color=080808&primary_color=000205",
+      dotColor: "#EBC3A4",
     },
     {
       name: "Terraza Pepita",
       url: "https://calendly.com/amphitryonterrazas/terraza-pepita?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=000306",
+      dotColor: "#7EB2FC",
     },
   ];
 
-  const [calendlyUrl, setCalendlyUrl] = useState(terrazas[0].url);
+  // const [calendlyUrl, setCalendlyUrl] = useState(terrazas[0].url);
 
-  const handleOptionClick = (newUrl: string) => {
-    setCalendlyUrl(newUrl);
-  };
+  // const handleOptionClick = (newUrl: string) => {
+  //   setCalendlyUrl(newUrl);
+  // };
 
   return (
     <div className="lg:min-h-none flex min-h-[600px] flex-col gap-4 lg:max-h-[472px] lg:justify-between xl:max-h-[660px]">
       <div className="flex items-center justify-center bg-[#EFEFEF] py-4 text-center lg:h-1/6">
         <h3>Revisa disponibilidad</h3>
       </div>
-      <div className="h-5/6">
+      <div className="h-4/6">
         <CustomCalendar />
         {/* <Calendly url={calendlyUrl} /> */}
       </div>
-      {/* <div className="flex items-center justify-around bg-[#EFEFEF] py-4 lg:h-1/6">
+      <div className="flex items-center justify-around bg-[#EFEFEF] py-4">
         {terrazas.map((terraza, index) => (
-          <label key={index}>
-            <input
-              type="radio"
-              name="terraza"
-              value={terraza.url}
-              checked={calendlyUrl === terraza.url}
-              onChange={() => handleOptionClick(terraza.url)}
-            />
-            {terraza.name}
-          </label>
+          <div className="flex items-center gap-1" key={index}>
+            <div
+              className={`relative h-4 w-4 rounded-full bg-[${terraza.dotColor}]`}
+            ></div>
+            <span className="text-sm lg:text-base">{terraza.name}</span>
+          </div>
         ))}
-      </div> */}
+
+        {/* <div className="h-4 w-4 rounded-full bg-[#7EB2FC]"></div> */}
+      </div>
     </div>
   );
 };
