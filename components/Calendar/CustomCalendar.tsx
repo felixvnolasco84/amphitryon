ade65381373ba6f44738c2790881a48839b0150b
Felix Vega <felix@polygonag.com>
2024-03-08 14:01:43 -0600

    Update calendar embed URL and add parent window script

--- components/Calendar/CustomCalendar.tsx
+++ components/Calendar/CustomCalendar.tsx
@@ -2,10 +2,17 @@ export default function CustomCalendar() {
   return (
     <div className="aspect-square h-full w-full overflow-x-scroll">
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=c_52a8099d5e40643984c12fa479d2c1532a429357fd679a952f2763ad759d469a%40group.calendar.google.com&ctz=America%2FMexico_City"
-        width="100%"
-        height="100%"
+        src="https://embed.styledcalendar.com/#bmU5KAjCRCAnaGZuVFld"
+        title="Styled Calendar"
+        className="styled-calendar-container"
+        style={{ width: "100%", border: "none" }}
+        data-cy="calendar-embed-iframe"
       ></iframe>
+      <script
+        async
+        type="module"
+        src="https://embed.styledcalendar.com/assets/parent-window.js"
+      ></script>
     </div>
   );
 }
