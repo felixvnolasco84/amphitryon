aab6d291ed6497b6dc0dacb2be5c9465c706fd33
Felix Vega <felix@polygonag.com>
2024-03-08 17:36:48 -0600

    Refactor terrazas array and update calendar component

--- components/Calendar/CalendarComponent.tsx
+++ components/Calendar/CalendarComponent.tsx
@@ -5,23 +5,23 @@ import Calendly from "./Calendly";
 import CustomCalendar from "./CustomCalendar";
 
 export const CalendarComponent = () => {
-  const terrazas = [
-    {
-      name: "La Galería",
-      url: "https://calendly.com/amphitryonterrazas/la-galeria?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=000409",
-      dotColor: "#A6E0DA",
-    },
-    {
-      name: "Macarella",
-      url: "https://calendly.com/amphitryonterrazas/macarella?hide_event_type_details=1&hide_gdpr_banner=1&text_color=080808&primary_color=000205",
-      dotColor: "#EBC3A4",
-    },
-    {
-      name: "Terraza Pepita",
-      url: "https://calendly.com/amphitryonterrazas/terraza-pepita?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=000306",
-      dotColor: "#7EB2FC",
-    },
-  ];
+  // const terrazas = [
+  //   {
+  //     name: "La Galería",
+  //     url: "https://calendly.com/amphitryonterrazas/la-galeria?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=000409",
+  //     dotColor: "#A6E0DA",
+  //   },
+  //   {
+  //     name: "Macarella",
+  //     url: "https://calendly.com/amphitryonterrazas/macarella?hide_event_type_details=1&hide_gdpr_banner=1&text_color=080808&primary_color=000205",
+  //     dotColor: "#EBC3A4",
+  //   },
+  //   {
+  //     name: "Terraza Pepita",
+  //     url: "https://calendly.com/amphitryonterrazas/terraza-pepita?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=000306",
+  //     dotColor: "#7EB2FC",
+  //   },
+  // ];
 
   // const [calendlyUrl, setCalendlyUrl] = useState(terrazas[0].url);
 
@@ -39,16 +39,20 @@ export const CalendarComponent = () => {
         {/* <Calendly url={calendlyUrl} /> */}
       </div>
       <div className="flex items-center justify-around bg-[#EFEFEF] py-4">
-        {terrazas.map((terraza, index) => (
-          <div className="flex items-center gap-1" key={index}>
-            <div
-              className={`relative h-4 w-4 rounded-full bg-[${terraza.dotColor}]`}
-            ></div>
-            <span className="text-sm lg:text-base">{terraza.name}</span>
-          </div>
-        ))}
+        <div className="flex items-center gap-1">
+          <div className="h-4 w-4 rounded-full bg-[#A6E0DA]"></div>
+          <span className="text-sm lg:text-base">La Galería</span>
+        </div>
 
-        {/* <div className="h-4 w-4 rounded-full bg-[#7EB2FC]"></div> */}
+        <div className="flex items-center gap-1">
+          <div className="h-4 w-4 rounded-full bg-[#EBC3A4]"></div>
+          <span className="text-sm lg:text-base">Macarella</span>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <div className="h-4 w-4 rounded-full bg-[#7EB2FC]"></div>
+          <span className="text-sm lg:text-base">Pepita</span>
+        </div>
       </div>
     </div>
   );
