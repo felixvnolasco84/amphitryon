3d90049f3e692b10103a8a975bff8a7b5b4821fc
Felix Vega <felix@polygonag.com>
2024-03-08 11:59:04 -0600

    Remove styled calendar iframe

--- components/Calendar/CustomCalendar.tsx
+++ components/Calendar/CustomCalendar.tsx
@@ -2,23 +2,11 @@ export default function CustomCalendar() {
   return (
     <div className="aspect-square h-full w-full overflow-x-scroll">
       <iframe
-        src="https://embed.styledcalendar.com/#qPD5Mc0TBU9sdPnIFFjt"
-        title="Styled Calendar"
-        className="styled-calendar-container"
-        style={{ width: "100%", border: "none" }}
-        data-cy="calendar-embed-iframe"
-      ></iframe>
-      <iframe
         className="mx-auto h-full w-full"
         src="https://calendar.google.com/calendar/embed?src=c_b8a50ca9fdb33abe461d74c935c0a544a3ae2c9aff5291b18a14cae55389ee86%40group.calendar.google.com&ctz=America%2FMexico_City"
         width="100%"
         height="100%"
       ></iframe>
-      <script
-        async
-        type="module"
-        src="https://embed.styledcalendar.com/assets/parent-window.js"
-      ></script>
     </div>
   );
 }
