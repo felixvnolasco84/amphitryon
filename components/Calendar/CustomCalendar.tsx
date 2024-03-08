f45e38198d2ea3f2ec93aaf2bc27c4090535a201
Felix Vega <felix@polygonag.com>
2024-03-08 13:32:13 -0600

    Update calendar source URL

--- components/Calendar/CustomCalendar.tsx
+++ components/Calendar/CustomCalendar.tsx
@@ -2,8 +2,7 @@ export default function CustomCalendar() {
   return (
     <div className="aspect-square h-full w-full overflow-x-scroll">
       <iframe
-        className="mx-auto h-full w-full"
-        src="https://calendar.google.com/calendar/embed?src=c_b8a50ca9fdb33abe461d74c935c0a544a3ae2c9aff5291b18a14cae55389ee86%40group.calendar.google.com&ctz=America%2FMexico_City"
+        src="https://calendar.google.com/calendar/embed?src=c_52a8099d5e40643984c12fa479d2c1532a429357fd679a952f2763ad759d469a%40group.calendar.google.com&ctz=America%2FMexico_City"
         width="100%"
         height="100%"
       ></iframe>
