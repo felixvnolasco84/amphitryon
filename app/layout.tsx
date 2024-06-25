be728becb87fe98e41d9cd754d8dbafa161c14ac
felixvnolasco84 <felix@polygonag.com>
2024-06-25 10:51:43 -0600

    chore: Update npm dependency to latest stable version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata, Viewport } from "next";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
+// import Script from "next/script";
 import "./globals.css";
 import { clashDisplayRegular } from "@/utils/fonts";
 import Header from "@/components/Header/Header";
@@ -21,7 +22,16 @@ export default function RootLayout({
   return (
     <html lang="es-Mx">
       <head>
-        <script
+        {/* <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z2FQ3KVBPJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z2FQ3KVBPJ');
+</script> */}
+        {/* <script
           defer
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -30,22 +40,23 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WNGXG4N4')`,
           }}
-        />
+        /> */}
       </head>
       <body className={`${clashDisplayRegular.className}`}>
         <div className="flex flex-col lg:gap-4">
           <Header />
-          <div id="root" className="flex flex-col flex-1 gap-4">
-            <iframe
+          <div id="root" className="flex flex-1 flex-col gap-4">
+            {/* <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-WNGXG4N4"
               height="0"
               width="0"
-            ></iframe>
+            ></iframe> */}
             {children}
           </div>
+          <GoogleAnalytics gaId="G-Z2FQ3KVBPJ" />
           <Footer />
         </div>
-        <Script></Script>
+        {/* <Script></Script> */}
       </body>
     </html>
   );
