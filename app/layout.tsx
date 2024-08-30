d118c6b42b510b44c3d27c08b228fd95e8306498
felixvnolasco84 <felix@polygonag.com>
2024-08-30 12:01:18 -0600

    chore: Remove commented out code and unused imports in app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,9 @@
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { WithContext, Organization } from "schema-dts";
 import Script from "next/script";
-// import Script from "next/script";
 import "./globals.css";
 import { clashDisplayRegular } from "@/utils/fonts";
 import Header from "@/components/Header/Header";
-import Head from "next/head";
 import Footer from "@/components/Footer/Footer";
 
 const jsonLd: WithContext<Organization> = {
@@ -52,35 +50,9 @@ export default function RootLayout({
       <head>
         <meta name="robots" content="index, follow" />
       </head>
-      {/* <head> */}
-      {/* <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z2FQ3KVBPJ"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-Z2FQ3KVBPJ');
-  </script> */}
-      {/* <script
-            defer
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-WNGXG4N4')`,
-            }}
-          /> */}
-      {/* </head> */}
       <body className={`${clashDisplayRegular.className}`}>
         <Header />
         <div id="root" className="flex flex-1 flex-col gap-3">
-          {/* <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WNGXG4N4"
-              height="0"
-              width="0"
-            ></iframe> */}
           <Script
             id="org-schema"
             type="application/ld+json"
@@ -91,9 +63,9 @@ export default function RootLayout({
           {children}
         </div>
         <GoogleAnalytics gaId="G-Z2FQ3KVBPJ" />
-        <GoogleTagManager gtmId="AW-16453854332" />
+        {/* <GoogleTagManager gtmId="AW-16453854332" /> */}
+        <GoogleTagManager gtmId="GTM-NBMB3DLT" />
         <Footer />
-        {/* <Script></Script> */}
       </body>
     </html>
   );
