e005e58dc13841078b3fd01dcce02c5095d0bd62
felixvnolasco84 <felix@polygonag.com>
2024-09-05 13:13:11 -0600

    feat: Update WhatsApp contact URL with message parameter

--- components/Flat/FlatComponent.tsx
+++ components/Flat/FlatComponent.tsx
@@ -11,6 +11,14 @@ export default function FlatComponent({
   flat: StaticImageData;
   flatPDF: string;
 }) {
+  const handleClick = () => {
+    TrackGoogleAnalyticsEvent(
+      "click",
+      "descargar_plano",
+      window.location.pathname
+    );
+  };
+
   return (
     <section>
       <h2 className="text-center text-2xl font-bold lg:text-4xl xl:mb-8 xl:text-5xl">
@@ -30,6 +38,7 @@ export default function FlatComponent({
         href={flatPDF}
         download="Plano.pdf"
         className="mt-6 flex justify-center"
+        onClick={handleClick}
       >
         <Button className="rounded-md bg-black px-4 py-2 text-white">
           Descargar Plano
--- components/Footer/FooterLinkComponent.tsx
+++ components/Footer/FooterLinkComponent.tsx
@@ -1,26 +1,43 @@
 "use client";
 
-import { TrackGoogleAnalyticsEvent } from "@/lib/google-analytics";
-import Link from "next/link";
+import { api } from "@/convex/_generated/api";
+import InitializeGoogleAnalytics, {
+  TrackGoogleAnalyticsEvent,
+} from "@/lib/google-analytics";
+import { useMutation } from "convex/react";
 import React from "react";
+import { Button } from "../ui/button";
 
 export default function FooterLinkComponent() {
+  const create = useMutation(api.contact.create);
+
+  const handleClick = () => {
+    InitializeGoogleAnalytics();
+    TrackGoogleAnalyticsEvent(
+      "click",
+      "enviar_mensaje",
+      window.location.pathname
+    );
+
+    const promise = create();
+
+    if (promise) {
+      promise.then((url) => {
+        if (url) {
+          window.open(url, "_blank");
+        }
+      });
+    }
+  };
+
   return (
-    <Link
-      target="_blank"
-      onClick={() =>
-        TrackGoogleAnalyticsEvent(
-          "click",
-          "enviar_mensaje",
-          window.location.pathname
-        )
-      }
-      className="text-left underline"
-      href={
-        "https://wa.me/5620244047?text=Hola,%20me%20gustaría%20saber%20más%20sobre%20sus%20servicios"
-      }
+    <Button
+      size={"default"}
+      variant={"ghost"}
+      className="rounded-md bg-transparent p-0 text-center text-lg text-white underline hover:bg-transparent hover:text-white"
+      onClick={handleClick}
     >
-      WHATSAPP
-    </Link>
+      WhatsApp
+    </Button>
   );
 }
