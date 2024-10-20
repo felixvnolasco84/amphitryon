e09b2f470d9cf5f999f824e6294e996590df83d9
felixvnolasco84 <felix@polygonag.com>
2024-10-20 12:02:29 -0600

    changed whatsapp button

--- components/WhatsApp/WhatsAppButton.tsx
+++ components/WhatsApp/WhatsAppButton.tsx
@@ -30,12 +30,19 @@ export default function WhatsAppButton({ title }: { title: string }) {
   };
 
   return (
-    <Button
-      size={"default"}
-      className="rounded-md bg-green-400 text-center text-xl text-gray-800 hover:bg-gray-800 hover:text-white"
-      onClick={handleClick}
+    <a
+      target="_blank"
+      className="inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md bg-green-400 px-4 py-2 text-center text-xl font-medium text-gray-800 ring-offset-background transition-colors hover:bg-gray-800 hover:text-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      href="https://api.whatsapp.com/send/?phone=5620244047&text=Hola%2C+me+gustar%C3%ADa+recibir+m%C3%A1s+informes.&type=phone_number&app_absent=0"
     >
-      {title}
-    </Button>
+      WhatsApp
+    </a>
+    // <Button
+    //   size={"default"}
+    //   className="rounded-md bg-green-400 text-center text-xl text-gray-800 hover:bg-gray-800 hover:text-white"
+    //   onClick={handleClick}
+    // >
+    //   {title}
+    // </Button>
   );
 }
