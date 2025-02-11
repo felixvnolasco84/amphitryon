77415ccdab0d42b90a6070bf338601c610deedcf
felixvnolasco84 <felix@polygonag.com>
2025-02-11 13:38:56 -0600

    added Salvador in contact round robin stategy for call button

--- convex/callContact.ts
+++ convex/callContact.ts
@@ -4,11 +4,13 @@ export const create = mutation({
   args: {},
   handler: async (ctx) => {
     const lastContact = await ctx.db.query("CallContact").order("desc").first();
-    const message = "Hola, me gustaría recibir más informes.";
     if (lastContact?.receivedBy === "Anton") {
       await ctx.db.insert("CallContact", { receivedBy: "Inés Torres" });
       return `tel:5523053711`;
     } else if (lastContact?.receivedBy === "Inés Torres") {
+      await ctx.db.insert("CallContact", { receivedBy: "Salvador" });
+      return `tel:5542957398`;
+    } else if (lastContact?.receivedBy === "Salvador") {
       await ctx.db.insert("CallContact", { receivedBy: "Anton" });
       return `tel:5620244047`;
     }
@@ -19,15 +21,15 @@ export const getLastContact = query({
   args: {},
   handler: async (ctx) => {
     const lastContact = await ctx.db.query("CallContact").order("desc").first();
-
     if (!lastContact) {
       return null;
     }
-
     if (lastContact?.receivedBy === "Inés Torres") {
       return `tel:5523053711`;
     } else if (lastContact?.receivedBy === "Anton") {
       return `tel:5620244047`;
+    } else if (lastContact?.receivedBy === "Salvador") {
+      return `tel:5542957398`;
     }
   },
 });
